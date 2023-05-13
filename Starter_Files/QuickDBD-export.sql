1516f89f0bd8e376a0c41ca6a22fd7590d502300
Gavin-Toole <121550462+Gavin-Toole@users.noreply.github.com>
2023-05-13 09:14:46 -0230

    database drawing and sql code

---
+++ Starter_Files/QuickDBD-export.sql
@@ -0,0 +1,56 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "category" (
+    "category" VARCHAR   NOT NULL,
+    "cagegory" CARCHAR   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHR   NOT NULL,
+    "subcategory" VARCHA   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "fist_name" VARCHAR   NOT NULL,
+    "last_nane" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    "goal" INTEGER   NOT NULL,
+    "pledge" INTEGER   NOT NULL,
+    "outcome" VARCHAR   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" VARCHAR   NOT NULL,
+    "start_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHER   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory" FOREIGN KEY("subcategory")
+REFERENCES "subcategory" ("subcategory_id");
+
