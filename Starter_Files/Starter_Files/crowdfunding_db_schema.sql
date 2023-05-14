ae0207d2f2653e2e9840765d212b2ce48e0513e1
Gavin-Toole <121550462+Gavin-Toole@users.noreply.github.com>
2023-05-14 11:47:48 -0230

    updated and added schema

---
+++ Starter_Files/Starter_Files/crowdfunding_db_schema.sql
@@ -0,0 +1,88 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Check if table exists and import category
+DROP TABLE IF EXISTS category CASCADE;
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR   NOT NULL,
+    "category" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+SELECT * FROM category;
+
+-- Check if table exists and import subcategory
+DROP TABLE IF EXISTS subcategory CASCADE;
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+SELECT * FROM subcategory;
+
+-- Check if table exists and import contacts
+DROP TABLE IF EXISTS contacts CASCADE;
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "fist_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+SELECT * FROM contacts;
+
+-- Check if table exists and import campaign
+DROP TABLE IF EXISTS campaign CASCADE;
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    "goal" INTEGER   NOT NULL,
+    "pledged" INTEGER   NOT NULL,
+    "outcome" VARCHAR   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" VARCHAR   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+	 CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+
+);
+
+SELECT * FROM campaign;
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory" FOREIGN KEY("subcategory")
+REFERENCES "subcategory" ("subcategory_id");
+
+-- Checking Table imports
+
+SELECT * from category;
+
+SELECT * from subcategory;
+
+SELECT * from contacts;
+
+SELECT * from campaign;
