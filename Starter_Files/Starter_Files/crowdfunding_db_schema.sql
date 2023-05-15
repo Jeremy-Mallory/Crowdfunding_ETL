6e439802f5219003d33f09d7633739f101d12808
Gavin-Toole <121550462+Gavin-Toole@users.noreply.github.com>
2023-05-15 16:09:09 -0230

    ready for review

--- Starter_Files/Starter_Files/crowdfunding_db_schema.sql
+++ Starter_Files/Starter_Files/crowdfunding_db_schema.sql
@@ -50,8 +50,8 @@ CREATE TABLE "campaign" (
     "contact_id" INT   NOT NULL,
     "company_name" VARCHAR   NOT NULL,
     "description" VARCHAR   NOT NULL,
-    "goal" INTEGER   NOT NULL,
-    "pledged" INTEGER   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
     "outcome" VARCHAR   NOT NULL,
     "backers_count" INTEGER   NOT NULL,
     "country" VARCHAR   NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE "campaign" (
     "end_date" DATE   NOT NULL,
     "category_id" VARCHAR   NOT NULL,
     "subcategory" VARCHAR   NOT NULL,
-	CONSTRAINT "pk-campaing" PRIMARY KEY(
+	CONSTRAINT "pk-campaign" PRIMARY KEY(
 	"cf_id"
 	)
 );
